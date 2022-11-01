3c584e80e15687f8c8e79c031f9150b5aeb62334
pablo di biasi <licdibiasi@gmail.com>
2022-11-01 18:57:40 -0300

    arreglando nombre cartwidget

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Cart } from 'react-bootstrap-icons'
 
-export default function CartWidjet() {
+export default function CartWidget() {
   return (
     <div>
       <Cart color="white" size={45} />
