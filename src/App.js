c101ceda83554e2241d1e96ee83086cc4c17751e
pablo di biasi <licdibiasi@gmail.com>
2022-11-22 12:20:46 -0300

    agregando collection a firestore con addDoc

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Cart from './components/Cart'
+import Checkout from './components/Checkout'
 import Contacto from './components/Contacto'
 import Footer from './components/Footer'
 import ItemDetailContainer from './components/ItemDetailContainer'
@@ -21,6 +22,7 @@ function App() {
             path="/categoria/:idcategoria"
             element={<ItemListContainer />}
           />
+          <Route path="/checkout" element={<Checkout />} />
           <Route path="/item/:iditem" element={<ItemDetailContainer />} />
           <Route path="/Contacto" element={<Contacto />} />
           <Route path="/Cart" element={<Cart />} />
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,8 +3,37 @@ import { useCartContext } from '../context/CartContext'
 import ItemCart from './ItemCart'
 import { Link } from 'react-router-dom'
 import { Button } from 'react-bootstrap'
+import { addDoc, collection, getFirestore } from 'firebase/firestore'
 export default function Cart() {
   const { cart, totalPrice } = useCartContext()
+
+  const order = {
+    buyer: {
+      nombre: 'pablo',
+      email: 'pablo@gmail.com',
+      tel: '5555555',
+      direccion: 'calle 1 555',
+    },
+    items: cart.map((product) => ({
+      id: product.id,
+      title: product.title,
+      price: product.price,
+      quantity: product.quantity,
+    })),
+    total: totalPrice(),
+  }
+
+  const handleClick = () => {
+    //instancia de firestore
+    const db = getFirestore()
+
+    //referencia a una collection
+    const ordersCollection = collection(db, 'orders')
+    //promesa addDoc
+
+    addDoc(ordersCollection, order).then(({ id }) => console.log(id))
+  }
+
   console.log(cart)
   if (cart.length === 0) {
     return (
@@ -35,7 +64,7 @@ export default function Cart() {
           {' '}
           <h2>Total: $ {totalPrice()}</h2>
           <Link to="/">
-            <Button> finalizar compra </Button>
+            <Button onClick={handleClick}> generar pedido </Button>
           </Link>
         </div>
       </div>
