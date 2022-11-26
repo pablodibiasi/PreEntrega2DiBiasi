1f3657626df85f42e37c0dbee37f6b1db9f72203
pablo di biasi <licdibiasi@gmail.com>
2022-11-26 15:57:42 -0300

    validando form de pedido, clear cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,45 +3,10 @@ import { useCartContext } from '../context/CartContext'
 import ItemCart from './ItemCart'
 import { Link } from 'react-router-dom'
 import { Button } from 'react-bootstrap'
-import { addDoc, collection, getFirestore } from 'firebase/firestore'
-
-import { useState } from 'react'
 
 export default function Cart() {
   const { cart, totalPrice } = useCartContext()
-  const [nombre, setNombre] = useState('')
-  const [email, setEmail] = useState('')
-  const [telefono, setTelefono] = useState('')
-  const [direccion, setDireccion] = useState('')
 
-  const order = {
-    buyer: {
-      nombre: nombre,
-      email: email,
-      telefono: telefono,
-      direccion: direccion,
-    },
-    items: cart.map((product) => ({
-      id: product.id,
-      title: product.title,
-      price: product.price,
-      quantity: product.quantity,
-    })),
-    total: totalPrice(),
-  }
-
-  const handleClick = () => {
-    //instancia de firestore
-    const db = getFirestore()
-
-    //referencia a una collection
-    const ordersCollection = collection(db, 'orders')
-    //promesa addDoc
-
-    addDoc(ordersCollection, order).then(({ id }) => console.log(id))
-  }
-
-  console.log(order)
   if (cart.length === 0) {
     return (
       <div style={{ textAlign: 'center', margin: '20px' }}>
@@ -59,44 +24,12 @@ export default function Cart() {
         {cart.map((product) => (
           <ItemCart key={product.id} product={product} />
         ))}
-        <div
-          style={{
-            textAlign: 'center',
-            margin: '20px',
-            border: '3px solid brown',
-            borderRadius: 5,
-            padding: 10,
-          }}
-        >
-          {' '}
-          <h2>Total a pagar: $ {totalPrice()}</h2>
-          <div>
-            <input
-              placeholder="Nombre"
-              value={nombre}
-              onChange={(e) => setNombre(e.target.value)}
-            />
-            <input
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              placeholder="Telefono"
-              value={telefono}
-              onChange={(e) => setTelefono(e.target.value)}
-            />
-            <input
-              placeholder="Direccion"
-              value={direccion}
-              onChange={(e) => setDireccion(e.target.value)}
-            />
-          </div>
-          <Link to="/">
-            <Button onClick={handleClick}> generar pedido </Button>
-          </Link>
-        </div>
       </div>
+      <h2>Total a pagar: $ {totalPrice()}</h2>
+      <Link to="/checkout">
+        {' '}
+        <Button> ir al checkout</Button>
+      </Link>
     </>
   )
 }
--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,5 +1,120 @@
 import React from 'react'
+import { useCartContext } from '../context/CartContext'
+
+import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
+import { addDoc, collection, getFirestore } from 'firebase/firestore'
+import swal from 'sweetalert'
+import { useState } from 'react'
 
 export default function Checkout() {
-  return <div>formulario de pago</div>
+  const { cart, totalPrice, clearCart } = useCartContext()
+  const [nombre, setNombre] = useState('')
+  const [email, setEmail] = useState('')
+  const [telefono, setTelefono] = useState('')
+  const [direccion, setDireccion] = useState('')
+  //validar email
+  function validateEmail(email) {
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
+    if (reg.test(email) == false) {
+      swal({
+        title: 'El formato de email no es valido',
+        text: 'ejemplo: nombre@mail.com',
+        icon: 'warning',
+        button: 'cerrar',
+        timer: 3000,
+      })
+      return false
+    }
+    return true
+  }
+
+  const order = {
+    buyer: {
+      nombre: nombre,
+      email: email,
+      telefono: telefono,
+      direccion: direccion,
+    },
+    items: cart.map((product) => ({
+      id: product.id,
+      title: product.title,
+      price: product.price,
+      quantity: product.quantity,
+    })),
+    total: totalPrice(),
+  }
+
+  const handleClick = () => {
+    //instancia de firestore
+    const db = getFirestore()
+
+    //referencia a una collection
+    const ordersCollection = collection(db, 'orders')
+    //promesa addDoc
+
+    if (!nombre || !email || !telefono || !direccion) {
+      swal({
+        title: 'completa los campos requeridos',
+        text: 'debes completar todos los campos',
+        icon: 'warning',
+        button: 'cerrar',
+        timer: 3000,
+      })
+      return
+    }
+    if (validateEmail(email) == false) {
+      return
+    }
+    addDoc(ordersCollection, order).then(
+      ({ id }) =>
+        swal({
+          icon: 'success',
+          title: 'pedido recibido con exito',
+          text: 'Su codigo de pedido es: ' + id,
+        }),
+      clearCart(),
+    )
+  }
+
+  console.log(order)
+
+  return (
+    <>
+      <div
+        style={{
+          textAlign: 'center',
+          margin: '20px',
+          border: '3px solid brown',
+          borderRadius: 5,
+          padding: 10,
+        }}
+      >
+        <h2>Total a pagar: $ {totalPrice()}</h2>
+        <div>
+          <input
+            placeholder="Nombre"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+          />
+          <input
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="Telefono"
+            value={telefono}
+            onChange={(e) => setTelefono(e.target.value)}
+          />
+          <input
+            placeholder="Direccion"
+            value={direccion}
+            onChange={(e) => setDireccion(e.target.value)}
+          />
+        </div>
+        <Button onClick={handleClick}> generar pedido </Button>
+      </div>
+    </>
+  )
 }
